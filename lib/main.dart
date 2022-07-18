dc1077071844c10500eba2cd07c704c509d306db
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-19 02:19:08 +0900

    ログインしたらhomeページに遷移するようにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:time_tracker_app/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_app/app/landing_page.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const SignInPage(),
+      home: const LandingPage(),
     );
   }
 }
