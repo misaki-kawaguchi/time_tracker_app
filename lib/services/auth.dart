992c09110bc70d45b96017142968ff91e9f483e4
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-23 14:56:07 +0900

    Googleサインインのメソッドを追加

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,13 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthBase {
   User? get currentUser;
   Stream<User?> authStateChanges();
   Future<User> signInAnonymously();
+  Future<User> signInWithGoogle();
   Future<void> signOut();
 }
 
-class Auth implements AuthBase{
+class Auth implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
 
   // ユーザーのサインイン状態の変更について通知する（サインインやサインアウト）
@@ -22,6 +24,35 @@ class Auth implements AuthBase{
   Future<User> signInAnonymously() async {
     final userCredential = await FirebaseAuth.instance.signInAnonymously();
     return userCredential.user!;
+  }
+
+  // Googleでサインインする
+  @override
+  Future<User> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _firebaseAuth.signInWithCredential(
+          GoogleAuthProvider.credential(
+            idToken: googleAuth.idToken,
+            accessToken: googleAuth.accessToken,
+          ),
+        );
+        return userCredential.user!;
+      } else {
+        throw FirebaseAuthException(
+          code: 'ERROR_MISSING_GOOGLE_ID_TOKEN',
+          message: 'Missing Google Id Token',
+        );
+      }
+    } else {
+      throw FirebaseAuthException(
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
   }
 
   @override
