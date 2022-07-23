b0dca66eeda9f79bb0a162c345c9ffc3a2e862dc
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-23 17:16:29 +0900

    signInWithGoogleメソッドを修正

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,9 +3,10 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthBase {
   User? get currentUser;
+
   Stream<User?> authStateChanges();
   Future<User> signInAnonymously();
-  Future<User> signInWithGoogle();
+  Future<void> signInWithGoogle();
   Future<void> signOut();
 }
 
@@ -28,31 +29,15 @@ class Auth implements AuthBase {
 
   // Googleでサインインする
   @override
-  Future<User> signInWithGoogle() async {
-    final googleSignIn = GoogleSignIn();
-    final googleUser = await googleSignIn.signIn();
-    if (googleUser != null) {
-      final googleAuth = await googleUser.authentication;
-      if (googleAuth.idToken != null) {
-        final userCredential = await _firebaseAuth.signInWithCredential(
-          GoogleAuthProvider.credential(
-            idToken: googleAuth.idToken,
-            accessToken: googleAuth.accessToken,
-          ),
-        );
-        return userCredential.user!;
-      } else {
-        throw FirebaseAuthException(
-          code: 'ERROR_MISSING_GOOGLE_ID_TOKEN',
-          message: 'Missing Google Id Token',
-        );
-      }
-    } else {
-      throw FirebaseAuthException(
-        code: 'ERROR_ABORTED_BY_USER',
-        message: 'Sign in aborted by user',
-      );
-    }
+  Future<void> signInWithGoogle() async {
+    final googleUser = await GoogleSignIn(scopes: ['profile', 'email']).signIn();
+    final googleAuth = await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   @override
