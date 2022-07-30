52592b652dc8aa2e97cf03fb4d7281e934f7a211
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-30 21:50:37 +0900

    EmailSignInFormBlocChangeNotifierに変更・リファクタリング

---
+++ lib/app/sign_in/email_sign_in_change_model.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:time_tracker_app/app/sign_in/email_sign_in_model.dart';
+import 'package:time_tracker_app/services/auth.dart';
+
+class EmailSignInChangeModel with ChangeNotifier{
+  EmailSignInChangeModel({
+    required this.auth,
+    this.email = '',
+    this.password = '',
+    this.formType = EmailSignInFormType.signIn,
+    this.isLoading = false,
+    this.submitted = false,
+  });
+
+  final AuthBase auth;
+  String email;
+  String password;
+  EmailSignInFormType formType;
+  bool isLoading;
+  bool submitted;
+
+  Future<void> submit() async {
+    updateWith(submitted: true, isLoading: true);
+    try {
+      if (formType == EmailSignInFormType.signIn) {
+        await auth.signInWithEmailAndPassword(email, password);
+      } else {
+        await auth.createUserWithEmailAndPassword(
+            email, password);
+      }
+    } catch (e) {
+      updateWith(submitted: false, isLoading: false);
+      rethrow;
+    }
+  }
+
+  String get primaryButtonText {
+    return formType == EmailSignInFormType.signIn
+        ? 'Sign in'
+        : 'Create an account';
+  }
+
+  String get secondaryButtonText {
+    return formType == EmailSignInFormType.signIn
+        ? 'Need an account? Register'
+        : 'Have an account? Sign in';
+  }
+
+  bool canSubmit(String email, String password) {
+    return email.isNotEmpty &&
+        password.isNotEmpty &&
+        !isLoading;
+  }
+
+  void toggleFormType() {
+    final formType = this.formType == EmailSignInFormType.signIn
+        ? EmailSignInFormType.register
+        : EmailSignInFormType.signIn;
+    updateWith(
+      email: '',
+      password: '',
+      formType: formType,
+      isLoading: false,
+      submitted: false,
+    );
+  }
+
+  void updateEmail(String email) => updateWith(email: email);
+
+  void updatePassword(String password) => updateWith(password: password);
+
+  void updateWith({
+    String? email,
+    String? password,
+    EmailSignInFormType? formType,
+    bool? isLoading,
+    bool? submitted,
+}){
+    this.email = email ?? this.email;
+    this.password = password ?? this.password;
+    this.formType = formType ?? this.formType;
+    this.isLoading = isLoading ?? this.isLoading;
+    this.submitted = submitted ?? this.submitted;
+    notifyListeners();
+  }
+}
