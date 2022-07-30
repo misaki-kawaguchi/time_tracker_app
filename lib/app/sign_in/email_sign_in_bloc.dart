de730a37f29a59a75625e03a64206807ac354ffd
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-30 19:41:55 +0900

    EmailSignInBlocにsubmitメソッドを追加・リファクタリング

--- lib/app/sign_in/email_sign_in_bloc.dart
+++ lib/app/sign_in/email_sign_in_bloc.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
-
 import 'package:time_tracker_app/app/sign_in/email_sign_in_model.dart';
+import 'package:time_tracker_app/services/auth.dart';
 
 class EmailSignInBloc {
+  EmailSignInBloc({required this.auth});
+  final AuthBase auth;
   final StreamController<EmailSignInModel> _modelController =
       StreamController<EmailSignInModel>();
 
@@ -11,6 +13,20 @@ class EmailSignInBloc {
 
   void dispose() {
     _modelController.close();
+  }
+
+  Future<void> submit() async {
+    updateWith(submitted: true, isLoading: true);
+    try {
+      if (_model.formType == EmailSignInFormType.signIn) {
+        await auth.signInWithEmailAndPassword(_model.email, _model.password);
+      } else {
+        await auth.createUserWithEmailAndPassword(_model.email, _model.password);
+      }
+    } catch (e) {
+      updateWith(submitted: false, isLoading: false);
+      rethrow;
+    }
   }
 
   void updateWith({
