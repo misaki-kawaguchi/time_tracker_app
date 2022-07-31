6543633f40f498d16228b237881d8e9415c04e49
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-31 14:15:26 +0900

    FirestoreServiceを作成

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:time_tracker_app/app/home/models/job.dart';
 import 'package:time_tracker_app/services/api_path.dart';
+import 'package:time_tracker_app/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
@@ -12,36 +13,18 @@ class FirestoreDatabase implements Database {
   FirestoreDatabase({required this.uid}) : assert(uid != null);
 
   final String uid;
+  final _service = FirestoreService.instance;
 
   @override
-  Future<void> createJob(Job job) => _setData(
+  Future<void> createJob(Job job) => _service.setData(
         path: APIPath.job(uid, 'job_abc'),
         data: job.toMap(),
       );
 
   @override
-  Stream<List<Job>> jobsStream() => _collectionStream(
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
     path: APIPath.jobs(uid),
     builder: (data) => Job.fromMap(data),
   );
 
-  Future<void> _setData({String? path, Map<String, dynamic>? data}) async {
-    final reference = FirebaseFirestore.instance.doc(path!);
-    print('$path: $data');
-    await reference.set(data!);
-  }
-
-  Stream<List<T>> _collectionStream<T>(
-      {required String path,
-      required T Function(Map<String, dynamic> data) builder}) {
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map(
-      (snapshot) => snapshot.docs
-          .map(
-            (snapshot) => builder(snapshot.data()),
-          )
-          .toList(),
-    );
-  }
 }
