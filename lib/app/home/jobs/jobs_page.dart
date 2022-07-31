7b6637306c5f9591b20f654a05bed90607373874
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-31 18:45:40 +0900

    ListViewリファクタリング

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,9 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_app/app/home/jobs/edit_job_page.dart';
 import 'package:time_tracker_app/app/home/jobs/empty_content.dart';
 import 'package:time_tracker_app/app/home/jobs/job_list_tile.dart';
+import 'package:time_tracker_app/app/home/jobs/list_items_builder.dart';
 import 'package:time_tracker_app/app/home/models/job.dart';
 import 'package:time_tracker_app/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker_app/services/auth.dart';
@@ -67,28 +67,12 @@ class JobsPage extends StatelessWidget {
     return StreamBuilder<List<Job>>(
       stream: database.jobsStream(),
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final jobs = snapshot.data!;
-          if (jobs.isNotEmpty) {
-            final children = jobs
-                .map((job) => JobListTile(
-              job: job,
-              onTap: () => EditJobPage.show(context, job: job),
-            ))
-                .toList();
-            return ListView(
-              children: children,
-            );
-          }
-          return const EmptyContent();
-        }
-        if (snapshot.hasError) {
-          return const Center(
-            child: Text('Some error occurred'),
-          );
-        }
-        return const Center(
-          child: CircularProgressIndicator(),
+        return ListItemBuilder<Job>(
+          snapshot: snapshot,
+          itemBuilder: (context, job) => JobListTile(
+            job: job,
+            onTap: () => EditJobPage.show(context, job: job),
+          ),
         );
       },
     );
