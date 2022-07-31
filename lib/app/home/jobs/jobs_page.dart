56d568a0c550455bd8a91fa94f29a4320c5845f6
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2022-08-01 00:04:59 +0900

    Entryの追加・更新・削除できるようにする (#15)
    * APIPathにentryとentriesを追加
    
    * サンプルファイルを追加
    
    * $に修正
    
    * keyを追加
    
    * JobEntriesPageを開くようにする
    
    * jobの追加と編集ページを開けるようにする
    
    * nullエラーを修正
    
    * 名前を変更
    
    * jobの変更をすぐに反映されるようにする

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker_app/app/home/job_entries/job_entries_page.dart';
 import 'package:time_tracker_app/app/home/jobs/edit_job_page.dart';
-import 'package:time_tracker_app/app/home/jobs/empty_content.dart';
 import 'package:time_tracker_app/app/home/jobs/job_list_tile.dart';
 import 'package:time_tracker_app/app/home/jobs/list_items_builder.dart';
 import 'package:time_tracker_app/app/home/models/job.dart';
@@ -58,7 +58,10 @@ class JobsPage extends StatelessWidget {
       ),
       body: _buildContents(context),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => EditJobPage.show(context),
+        onPressed: () => EditJobPage.show(
+          context,
+          database: Provider.of<Database>(context, listen: false),
+        ),
         child: const Icon(Icons.add),
       ),
     );
@@ -80,7 +83,7 @@ class JobsPage extends StatelessWidget {
             onDismissed: (direction) => _delete(context, job),
             child: JobListTile(
               job: job,
-              onTap: () => EditJobPage.show(context, job: job),
+              onTap: () => JobEntriesPage.show(context, job),
             ),
           ),
         );
