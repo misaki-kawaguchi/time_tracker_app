fda6ff219a61c23094c8f80432c50837891f0c72
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2022-07-31 19:45:55 +0900

    Jobを削除できるようにする・ListViewの調整 (#14)
    * jobのデータがない場合EmptyContentを表示する
    
    * ListViewリファクタリング
    
    * ListViewにボーダーを追加
    
    * スワイプしてjobを削除できるようにする

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,10 +1,13 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_app/app/home/jobs/edit_job_page.dart';
+import 'package:time_tracker_app/app/home/jobs/empty_content.dart';
 import 'package:time_tracker_app/app/home/jobs/job_list_tile.dart';
+import 'package:time_tracker_app/app/home/jobs/list_items_builder.dart';
 import 'package:time_tracker_app/app/home/models/job.dart';
 import 'package:time_tracker_app/common_widgets/show_alert_dialog.dart';
+import 'package:time_tracker_app/common_widgets/show_exception_alert_dialog.dart';
 import 'package:time_tracker_app/services/auth.dart';
 import 'package:time_tracker_app/services/database.dart';
 
@@ -66,27 +69,35 @@ class JobsPage extends StatelessWidget {
     return StreamBuilder<List<Job>>(
       stream: database.jobsStream(),
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final jobs = snapshot.data!;
-          final children = jobs
-              .map((job) => JobListTile(
-                    job: job,
-                    onTap: () => EditJobPage.show(context, job: job),
-                  ))
-              .toList();
-          return ListView(
-            children: children,
-          );
-        }
-        if (snapshot.hasError) {
-          return const Center(
-            child: Text('Some error occurred'),
-          );
-        }
-        return const Center(
-          child: CircularProgressIndicator(),
+        return ListItemBuilder<Job>(
+          snapshot: snapshot,
+          itemBuilder: (context, job) => Dismissible(
+            background: Container(
+              color: Colors.red,
+            ),
+            key: Key('job-${job.id}'),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) => _delete(context, job),
+            child: JobListTile(
+              job: job,
+              onTap: () => EditJobPage.show(context, job: job),
+            ),
+          ),
         );
       },
     );
+  }
+
+  Future<void> _delete(BuildContext context, Job job) async {
+    try {
+      final database = Provider.of<Database>(context, listen: false);
+      await database.deleteJob(job);
+    } on FirebaseException catch (e) {
+      showExceptionAlertDialog(
+        context,
+        title: 'Operation failed',
+        exception: e,
+      );
+    }
   }
 }
